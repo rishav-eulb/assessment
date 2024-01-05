1e7524b9b2a873fc3f883cedc8061d0583d2246e
rishav-eulb <77015951+rishav-eulb@users.noreply.github.com>
2024-01-05 13:07:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 // config.js
 module.exports = {
   jwtSecret:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJpZCI6IjY1OTc3Y2VmYTljZTFiODA2ZjljMDQxNiIsInVzZXJuYW1lIjoicmoiLCJpYXQiOjE3MDQ0MjkxNjZ9YLCYl5UmdIJVDbYrcZvlAERYydwBh8aYueS7DOMR2ac",
-  jwtAlgorithm: 'HS256'  // or the algorithm you are using (e.g., 'RS256' for RSA)
-
-
+    "enter your secret key"
 };
